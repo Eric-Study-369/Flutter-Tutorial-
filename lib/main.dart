910bae334d2ef73bb53ef824987944f31818679e
Eric-study-369 <jakeson270@gmail.com>
2024-06-21 18:58:12 +0700

    fetch(course): 31:33 - Column properties

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,49 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Dog App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Yelow lab'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              DecoratedBox(decoration: BoxDecoration(color: Colors.lightBlueAccent),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('Rocky'),
+              ),
+              ),
+              DecoratedBox(
+                decoration: BoxDecoration(color: Colors.lightBlueAccent),
+                child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('Sport'),
+                ),
+              ),
+              DecoratedBox(
+                decoration: BoxDecoration(color: Colors.lightGreenAccent),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text('Fido'),
+                ),
+                ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/*
 class MyApp extends StatelessWidget{
 
   @override 
@@ -27,3 +70,4 @@ class MyApp extends StatelessWidget{
 
   }
 }
+*/
