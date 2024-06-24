70380311532dff19b32283b577b1ed6337563980
Eric-study-369 <jakeson270@gmail.com>
2024-06-24 15:43:21 +0700

    add file

--- lib/main.dart
+++ lib/main.dart
@@ -16,55 +16,53 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(context){
-    return Scaffold(
+    return Scaffold( 
+      backgroundColor: Colors.white,
+     drawer: Container(
+          width: 400,
+          height: 500,
+          color: Colors.white,
+      ), 
       appBar: AppBar(
        title: Text('My Frist App'),
+       backgroundColor: Colors.brown,
         centerTitle: true,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context) => SecondScreen()));
+          //Navigator.push(context, MaterialPageRoute(builder: (context) => SecondScreen()));
         },
-      ),
+        child: Text('Click'),
+        backgroundColor: Colors.black,
+        
+
+      ), 
       body: Column(
+        
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text('hellow gfg '),
-          Text('New text'),
-          Text('New text'),
-        ],
-      )
+          Text('Hello, Welcomback! '),
+          Text('Pleas, login to join or continuse'),
+          TextField(
+            decoration: InputDecoration(hintText: 'UserName'),
+          ),
+          TextField(
+            decoration: InputDecoration(hintText: 'Enter password'),
+            ),
+          TextButton(onPressed: (){
+              print('forgot pasword is Chlicked');
+              //Text('show data ');
+          }, child: Text('Forgot password')), 
+          ElevatedButton(
+            onPressed: (){
+              print('login si chekeds');
+            }, 
+            child: Text('Log in')),
+            Text('or login with '), 
 
-    );
-  }
-}
-
-class SecondScreen extends StatelessWidget{
-
-  @override
-  Widget build(context){
-    return Scaffold(
-      appBar: AppBar(
-       title: Text('My Secondd App'),
-        centerTitle: true,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-
-        },
-      ),
-      body: Column(
-        children: [
-          Text('hellow gfg '),
-          Text('New text'),
-          Text('New text'),
-          Text('New text'),
-          Text('New text'),
-          Text('New text'),
-          Text('New text'),
-          Text('New text'),
-          Text('New text'),
-          Text('New text'),
-          Text('New text'),
+            ElevatedButton(onPressed: (){
+              print('login with google cleck');
+            }, child: Text('Login With Google')),
         ],
       )
 
